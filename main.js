721ddb0aa5b97b442a06e6820bb86ee3346fc7f1
Norbert Witkowski <norbert@fooz.pl>
2025-06-20 14:39:51 +0200

    Add SCSS styling

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const os = require ('os')
 
 function createWindow () {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 350,
+        height: 500,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
