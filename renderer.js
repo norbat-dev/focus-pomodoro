85d035c1a4a55355fa8da9d5514bf75fddce31d1
Norbert Witkowski <norbat@norbat.dev>
2025-07-19 01:12:11 +0200

    Attach new task to list after create

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     const username = await window.api.getUsername();
     const userNameHolder = document.getElementById('user-name');
     userNameHolder.textContent = username;
+    const taskList = document.getElementById('task-list')
 
 
     // const todayTasks = await window.api.getTodayTask();
@@ -9,20 +10,34 @@ window.addEventListener('DOMContentLoaded', async () => {
         if (tasks.error) {
             console.error('Błąd:', tasks.error);
         } else {
-            let taskWrapper = document.getElementById('task-list');
-            let taskTemplate = document.getElementById('task-template');
+            const taskWrapper = document.getElementById('task-list');
+            const taskTemplate = document.getElementById('task-template');
             tasks.forEach( task => {
                 const clone = taskTemplate.content.cloneNode(true);
                 clone.querySelector('.task-item__text').textContent = task.title;
                 clone.querySelector('.task-item__time').textContent = task.time;
-                taskWrapper.appendChild(clone);
+                // taskWrapper.appendChild(clone);
+                taskWrapper.insertBefore(clone, taskWrapper.firstChild);
             });
         }
     });
 
+    taskList.addEventListener("click", (event) => {
+        console.log(event.target);
+        // if(event.target.tagName === 'BUTTON') {
+        //     console.log(event.target);
+        // }
+    })
 });
 
 document.getElementById('add-task-submit').addEventListener('click', async () => {
+    const taskWrapper = document.getElementById('task-list');
+    const taskTemplate = document.getElementById('task-template');
     const title = document.getElementById('add-task-input').value;
     const add = await window.api.addTask( title );
+    const clone = taskTemplate.content.cloneNode(true);
+    clone.querySelector('.task-item__text').textContent = title;
+    clone.querySelector('.task-item__time').textContent = '00:00:00';
+    // taskWrapper.appendChild(clone);
+    taskWrapper.insertBefore(clone, taskWrapper.firstChild);
 });
